e6a719999dd32ac139c23085884c73be285e9d63
benzed3 <benzed3@gmail.com>
2021-10-26 16:25:00 -0400

    added javascript for drag and drop

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 //for drag and drop//
 
+function myFunction(event) {
+    var x = event.touches[0].clientX;
+    var y = event.touches[0].clientY;
+    document.getElementById("demo").innerHTML = "x=" + x + ", y=" + y;
+}
+
+//for click and drag kenshin picture
+function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+}
