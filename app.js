47215986d39a9f26df5098741ec83ce775b73d87
benzed3 <benzed3@gmail.com>
2021-12-05 13:07:59 -0500

    deleted unnecessary code

--- app.js
+++ app.js
@@ -15,20 +15,3 @@ function drop(event) {
 }
 
 //for touch drag
-var whiterookA = document.getElementById("wRookA");
-
-whiterookA.addEventListener("touchmove", function (ev) {
-
-    var touchLocation = ev.targetTouches[0];
-
-    whiterookA.style.left = touchLocation.pageX + "px";
-    whiterookA.style.top = touchLocation.pageY + "px";
-
-})
-
-whiterookA.addEventListener("touchend", function (ev) {
-
-    var x = parseInt(whiterookA.style.left);
-    var y = parseInt(whiterookA.style.top);
-
-})
