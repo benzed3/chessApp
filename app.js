32093f79b4ed111255df325e3a42764f2ff9c28b
benzed3 <benzed3@gmail.com>
2021-12-11 19:43:37 -0500

    added draggin code to black rook b and positioned it

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-//for click and drag//
-
+//for click and drag
 //black rook 1//
 dragElement(document.getElementById("rookdiv"));
 
@@ -18,6 +17,45 @@ function dragElement(elmnt) {
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
+
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+//black rook 2//
+dragElement(document.getElementById("rook2div"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "rookb")) {
+        document.getElementById(elmnt.id + "rookb").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+
         document.onmousemove = elementDrag;
     }
 
