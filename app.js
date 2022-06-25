2cccd9333c1255d47e84e0dc3234e8fe061e2eaa
benzed3 <benzed3@gmail.com>
2022-06-25 18:32:00 -0400

    adding click and drag code to black bishop 2

--- app.js
+++ app.js
@@ -186,3 +186,41 @@ function dragElement(elmnt) {
         document.onmousemove = null;
     }
 }
+
+//click and drag for black bishop 2//
+dragElement(document.getElementById("bb2"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "blackBishop2")) {
+
+        document.getElementById(elmnt.id + "blackBishop2").onmousedown = dragMouseDown;
+    } else {
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
